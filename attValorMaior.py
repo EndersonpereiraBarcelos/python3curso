6203f6362d0fb4265c1961ac4809bb41c548cf77
endersonpb <endersonpb0@gmail.com>
2025-04-10 16:44:32 -0300

    up

---
+++ attValorMaior.py
@@ -0,0 +1,8 @@
+numero_1 = input("informe 1 numero: ")
+numero_2 = input("Informe outro numero: ")
+
+if numero_1 >= numero_2:
+    print(f'Número {numero_1=} maior que o primeiro valor {numero_2=} ')
+
+elif numero_2 >= numero_2:
+    print(f'Número {numero_2=} maior que o primeiro valor {numero_1=} ')
